c28bcaa442d8570da376b6c36aceb339a5746e0b
Rushi <chappidi.rushi@docengage.in>
2022-06-27 22:14:51 +0530

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.sendStatus(200).send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!').sendStatus(200))
+app.get('/alive', (req, res) => res.send('Yes You are alive').sendStatus(200))
 app.listen(process.env.PORT || 5000, () => console.log(`Example app listening on port ${port}!`));
 module.exports = app;
